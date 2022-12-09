1c354fc49265b849811e16211708737728793406
Irene Abril Cabezas <79768254+iabrilcabezas@users.noreply.github.com>
2022-12-09 09:38:39 -0800

    no plotting in bbpower, i can do my own plotting thnks

--- utils/bbpw_script.py
+++ utils/bbpw_script.py
@@ -10,7 +10,7 @@ from utils.params import PATH_DICT, NAME_RUN
 def write_config_yml_script(type_cov, nside_bbpw):
 
     '''
-    Writes .yml file used by BBPower
+    Writes .yml file used by BBPower in BBCompSep (no plotting!)
 
     * Parameters *
     type_cov: 'w' or 'wt'
@@ -27,8 +27,7 @@ def write_config_yml_script(type_cov, nside_bbpw):
     dict_config = {'global': {'nside': nside_bbpw, 'compute_dell': False},
                    'modules': 'bbpower',
                    'launcher': 'local',
-                   'stages': [{'name': 'BBCompSep', 'nprocess': 1},
-                              {'name': 'BBPlotter', 'nprocess': 1}],
+                   'stages': [{'name': 'BBCompSep', 'nprocess': 1}], #{'name': 'BBPlotter', 'nprocess': 1}],
                    'inputs': {'cells_coadded': path_inputs + '_tot.fits',
                               'cells_fiducial': path_inputs + '_fid.fits',
                               'cells_noise': path_inputs + '_noi.fits'},
@@ -37,7 +36,7 @@ def write_config_yml_script(type_cov, nside_bbpw):
                    'output_dir': PATH_DICT['output_path'] + 'outputs/',
                    'log_dir': PATH_DICT['output_path'] + 'outputs/',
                    'pipeline_log': PATH_DICT['output_path'] + 'outputs/log.txt',
-                                     'BBCompSep': {'sampler': 'emcee',
+                   'BBCompSep': {'sampler': 'emcee',
                                  'nwalkers': 24,
                                  'n_iters': 1000,
                                  'likelihood_type': 'h&l',
@@ -57,11 +56,12 @@ def write_config_yml_script(type_cov, nside_bbpw):
                                                               'cl_parameters': {'BB': {'amp_d_bb': ['amp', 'tophat', [0.0, 5.0, 'inf']],
                                                                                        'alpha_d_bb': ['alpha', 'tophat', [-1.0, -0.2, 0.0]],
                                                                                        'l0_d_bb': ['ell0', 'fixed', [80.0]]}}}}},
-                   'BBPlotter': {'lmax_plot': 120,
-                                 'plot_coadded_total': False,
-                                 'plot_noise': False,
-                                 'plot_nulls': False,
-                                 'plot_likelihood': True}}
+                #    'BBPlotter': {'lmax_plot': 120,
+                #                  'plot_coadded_total': False,
+                #                  'plot_noise': False,
+                #                  'plot_nulls': False,
+                #                  'plot_likelihood': True}
+                                 }
 
     with open(path_config, 'w') as file:
         yaml.dump(dict_config, file, default_flow_style=True)
