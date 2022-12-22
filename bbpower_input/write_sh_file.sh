642ebee31a04a4e17dd2f6c70d64719481389abc
Irene Abril Cabezas <79768254+iabrilcabezas@users.noreply.github.com>
2022-12-22 13:28:58 -0800

    better naming scheme, automatize better

--- bbpower_input/write_sh_file.sh
+++ bbpower_input/write_sh_file.sh
@@ -4,34 +4,43 @@ base_folder=/global/cfs/cdirs/act/data/iabril/BBPower/pipeline_test
 #input_folder=/global/common/software/act/iabril/python/DustNGwBBP/bbpower_input
 
 machine=cori
-w_type=wt
 experiment=so
 nside=256
 lmin=2
 nbands=50
 dell=10
-smooth=40.0
 mtype=full
 apodeg=nan
+smooth=20.0
+dellnmt=10
+pol=B
+weight=Cl
 
-test_type=${experiment}_full/$w_type
-config_type=bbpw_${experiment}_full_${w_type}
-name_run=${machine}_${experiment}_${nside}_${lmin}_${nbands}_${dell}_${mtype}_${apodeg}_${smooth}_10_B
+ctype=dcs
+#w_type=wt
+cross=C
+moments=M
 
-name_config=${name_run}_all_Cl_${w_type}
-name_chain=${experiment}_${nside}_${lmin}_${nbands}_${dell}_${mtype}_${apodeg}_${smooth}_${w_type}
+all=_tot.fits
+noise=_noi.fits 
+fid=_fid.fits
 
-all=_all_Cl_${w_type}_tot.fits
-noise=_all_Cl_${w_type}_noi.fits 
-fid=_all_Cl_${w_type}_fid.fits
+name_run=${machine}_${experiment}_${nside}_${lmin}_${nbands}_${dell}_${mtype}_${apodeg}_${smooth}_${dellnmt}_${pol}_${ctype}_${weight}
 
-if [ -f $base_folder/chains/${name_chain}.npz ]; then
-    rm $base_folder/chains/${name_chain}.npz*
-fi
+wtypes='w wt'
 
-# Run pipeline
-python -m bbpower BBCompSep --cells_coadded=$base_folder/$name_run$all  --cells_noise=$base_folder/$name_run$noise --cells_fiducial=$base_folder/$name_run$fid    --config=$base_folder/config_files/${name_config}.yml      --param_chains=$base_folder/chains/${name_chain}.npz       --config_copy=$base_folder/temp/config_copy.yml
+for w_type in $wtypes
+do
+    name_c=${name_run}_${cross}_${moments}_${w_type}
+    name_cls=${name_run}_${w_type}
 
+    if [ -f $base_folder/chains/${name_c}.npz* ]; then
+        rm $base_folder/chains/${name_c}.npz*
+    fi
+
+    # Run pipeline
+    python -m bbpower BBCompSep --cells_coadded=$base_folder/${name_cls}${all}  --cells_noise=$base_folder/${name_cls}${noise} --cells_fiducial=$base_folder/${name_cls}${fid}    --config=$base_folder/config_files/${name_c}.yml      --param_chains=$base_folder/chains/${name_c}.npz       --config_copy=$base_folder/temp/config_copy.yml
+done
 # This plots the results of the pipeline
 # i'll do the plotting myself, thank you
 # python -m bbpower BBPlotter  --cells_coadded_total=$base_folder/$name_run$all  --cells_coadded=$base_folder/$name_run$all   --cells_null=$base_folder/$name_run$all     --cells_noise=$base_folder/$name_run$noi        --cells_fiducial=$base_folder/$name_run$fid           --param_chains=$base_folder/chains/${name_chain}.npz            --plots=$base_folder/temp/plots                  --plots_page=$base_folder/temp/plots/plots_page.html                     --config=$base_folder/config_files/${name_config}.yml
