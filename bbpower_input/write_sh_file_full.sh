f97457fdbd9272c2b74e1ee86c756f468f21115a
Irene Abril Cabezas <79768254+iabrilcabezas@users.noreply.github.com>
2023-01-17 02:58:26 -0800

    different runs

---
+++ bbpower_input/write_sh_file_full.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+base_folder=/global/cfs/cdirs/act/data/iabril/BBPower/pipeline_test
+#input_folder=/global/common/software/act/iabril/python/DustNGwBBP/bbpower_input
+
+machine=perl
+experiment=so
+nside=256
+lmin=2
+nbands=50
+dell=10
+dellnmt=10
+pol=B
+weight=Cl
+
+mtype=full
+apodeg=nan
+
+#smooth=40.0
+#w_type=wt
+#ctype=dcs
+cross=0
+moments=0
+
+bands=all
+lminbb=30
+lmaxbb=300
+
+all=_tot.fits
+noise=_noi.fits 
+fid=_fid.fits
+
+wtypes='w wt'
+ctypes='dc0 dcs'
+mmts='0 M'
+crosses='0 C'
+smoothes=( "10.0" "20.0" "40.0")
+
+for ctype in $ctypes
+do
+for smooth in "${smoothes[@]}"
+do
+# for cross in $crosses
+# do
+#for moments in $mmts
+#do
+for w_type in $wtypes
+do
+
+    name_run=${experiment}_${nside}_${lmin}_${nbands}_${dell}_${mtype}_${apodeg}_${smooth}_${dellnmt}_${pol}_${ctype}_${weight}
+    name_c=${name_run}_${cross}_${moments}_${w_type}_${lminbb}_${lmaxbb}_${bands}
+    name_cls=${name_run}_${w_type}
+    echo ${name_c}
+
+    if [ -f ${base_folder}/chains/${name_c}.npz.h5 ]; then
+        rm ${base_folder}/chains/${name_c}.npz*
+    fi
+
+    # Run pipeline
+    python -m bbpower BBCompSep --cells_coadded=$base_folder/${name_cls}${all}  --cells_noise=$base_folder/${name_cls}${noise} --cells_fiducial=$base_folder/${name_cls}${fid}    --config=$base_folder/config_files/${name_c}.yml      --param_chains=$base_folder/chains/${name_c}.npz       --config_copy=$base_folder/temp/config_copy.yml
+done
+#done
+#done
+done
+done
+# This plots the results of the pipeline
+# i'll do the plotting myself, thank you
+# python -m bbpower BBPlotter  --cells_coadded_total=$base_folder/$name_run$all  --cells_coadded=$base_folder/$name_run$all   --cells_null=$base_folder/$name_run$all     --cells_noise=$base_folder/$name_run$noi        --cells_fiducial=$base_folder/$name_run$fid           --param_chains=$base_folder/chains/${name_chain}.npz            --plots=$base_folder/temp/plots                  --plots_page=$base_folder/temp/plots/plots_page.html                     --config=$base_folder/config_files/${name_config}.yml
+
+# # Check the final plots exist
+# if [ ! -f $base_folder/temp/plots/triangle.png ]; then
+#     echo "Test did not pass"
+# else
+#     echo "Test passed"
+# fi
--- test.py
+++ test.py
@@ -1,35 +1,35 @@
 '''
 first step
 '''
-#from utils.params import config
+from utils.params import config
 from utils.params import NAME_COMP
 from utils_bbpw.bbpw_script import write_config_yml_script
 from utils_bbpw.params import dict_params_bbpw, dict_ells_bbpw, niterbands
-#from dustngwbbp.compute_cl import compute_cl_nobin, compute_cl
-#from dustngwbbp.compute_couplingmatrix import compute_couplingmatrix
-#from dustngwbbp.compute_cov import compute_cov, get_effective_cov
+from dustngwbbp.compute_cl import compute_cl_nobin, compute_cl
+from dustngwbbp.compute_couplingmatrix import compute_couplingmatrix
+from dustngwbbp.compute_cov import compute_cov, get_effective_cov
 
-# compute_couplingmatrix(**config.mask_param.__dict__)
+# w2_mean = compute_couplingmatrix(**config.mask_param.__dict__)
 
 # compute_cl_nobin('d00')
 # compute_cl_nobin(NAME_COMP)
 
-# compute_cov('d00')
-# compute_cov(NAME_COMP)
+# compute_cov('d00', w2_mean)
+# compute_cov(NAME_COMP, w2_mean)
 
 # get_effective_cov()
 
 # compute_cl(NAME_COMP,'w')
 # compute_cl(NAME_COMP,'wt')
 
-# for biter in niterbands:
-#     dict_ells_bbpw['bands'] = biter
-#     for template in ['w', 'wt']:
-#         write_config_yml_script(template, params_bbpw = dict_params_bbpw,
-#                                     dict_compsep= dict_ells_bbpw,
-#                                     cros = False, mmt = False) # dc0 or dcs, simple
-#         if NAME_COMP == 'dcs':
-#             for moment in [True, False]:
-#                 write_config_yml_script(template, params_bbpw = dict_params_bbpw,
-#                                     dict_compsep= dict_ells_bbpw,
-#                                     cros = True, mmt = moment)
+for biter in niterbands:
+    dict_ells_bbpw['bands'] = biter
+    for template in ['w', 'wt']:
+        write_config_yml_script(template, params_bbpw = dict_params_bbpw,
+                                    dict_compsep= dict_ells_bbpw,
+                                    cros = False, mmt = False) # dc0 or dcs, simple
+        if NAME_COMP == 'dcs':
+            for moment in [True, False]:
+                write_config_yml_script(template, params_bbpw = dict_params_bbpw,
+                                    dict_compsep= dict_ells_bbpw,
+                                    cros = True, mmt = moment)
