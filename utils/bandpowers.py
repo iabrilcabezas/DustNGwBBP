ebd80caa7237a4cb864f279c740c2c11eefccc64
Irene Abril Cabezas <79768254+iabrilcabezas@users.noreply.github.com>
2023-03-02 01:58:31 -0800

    fix inconsistent ell eff

--- utils/bandpowers.py
+++ utils/bandpowers.py
@@ -33,7 +33,7 @@ def get_ell_arrays(lmin, dell, nbands):
     lmax = lmin + dell * nbands
     larr_all = np.arange(lmax+1)
     lbands = np.linspace(lmin,lmax,nbands+1,dtype=int)
-    leff = 0.5*(lbands[1:]+lbands[:-1])
+    leff = [np.mean(np.arange(x,y)) for x,y in zip(lbands[:-1], lbands[1:])] # 0.5*(lbands[1:]+lbands[:-1])
 
     return (lmax, larr_all, lbands, leff)
 
