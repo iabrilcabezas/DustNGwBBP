813e2c2ceb9adb69c09226df2c85977b1f68c622
Irene Abril Cabezas <79768254+iabrilcabezas@users.noreply.github.com>
2023-03-15 08:56:01 -0700

    add df** covs

--- run_chi2andpte.py
+++ run_chi2andpte.py
@@ -6,7 +6,7 @@ chi2 and PTE calculation
 from pte.calc_chi2pte import get_chi2andpte
 from utils_bbpw.params import dict_ells_bbpw, niterbands
 
-for type_cov in ['wt' , 'df00', 'dfwt']:
+for type_cov in ['wt' , 'df00', 'dfwt', 'dfwtm']:
     print(type_cov)
     #iterate over all band combinations
     for biter in niterbands:
