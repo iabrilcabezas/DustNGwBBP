6e0d0f261af4a92dede7cbb454df75da322f6b30
Irene Abril Cabezas <79768254+iabrilcabezas@users.noreply.github.com>
2023-03-17 10:13:08 -0700

    specifies binning

--- run_dustfil.py
+++ run_dustfil.py
@@ -2,37 +2,37 @@
 runs computation of covariance from sims, and merges to exisiting cov from other components
 '''
 
-from dustfilaments.compute_cov import compute_cell_dustfil
+#from dustfilaments.compute_cov import compute_cell_bin_dustfil
 from dustfilaments.compute_cov import calibrate_cells, compute_full_cov, merge_cov
 from dustfilaments.compute_cov import compute_cov_fromsims, compute_tildecov
-from utils_dustfil.params import DF_NSIMS, TYPE_DCOV
+from dustfilaments.compute_cov import compute_cell_nobin_dustfil
+from utils.params import DF_NSIMS, NSIDE
 from dustngwbbp.compute_cl import compute_cl
 from utils_bbpw.bbpw_script import write_config_yml_script
 from utils_bbpw.params import dict_ells_bbpw, dict_params_bbpw
 
-# compute_cell_dustfil(DF_NSIMS)
-calibrate_cells()
+TYPE_DCOV = 'dfwtm'
 
-compute_cov_fromsims('small')
-compute_tildecov('small')
-compute_tildecov('all')
+# # compute_cell_bin_dustfil(DF_NSIMS, NSIDE)
+# compute_cell_nobin_dustfil(DF_NSIMS, NSIDE)
+# calibrate_cells('nobin')
 
-merge_cov('david')
+# compute_cov_fromsims('small', 'nobin')
 
-compute_full_cov(TYPE_DCOV)
+# compute_tildecov('small', 'nobin')
+# compute_tildecov('all', 'nobin')
+
+if TYPE_DCOV != 'df00':
+
+    merge_cov(TYPE_DCOV, 'nobin')
+
+compute_full_cov(TYPE_DCOV, 'nobin')
 
 compute_cl('dcs',TYPE_DCOV)
 
 dict_ells_bbpw['bands'] = 'all'
 
-write_config_yml_script(TYPE_DCOV, params_bbpw = dict_params_bbpw,
+for mom, decs in zip([False, False, True],[False, True, False]):
+    write_config_yml_script(TYPE_DCOV, params_bbpw = dict_params_bbpw,
                             dict_compsep= dict_ells_bbpw,
-                            cros = False, mmt = False, dec = False) # dc0 or dcs, simple
-
-write_config_yml_script(TYPE_DCOV, params_bbpw = dict_params_bbpw,
-                            dict_compsep= dict_ells_bbpw,
-                            cros = False, mmt = False, dec = True) # dc0 or dcs, simple
-
-write_config_yml_script(TYPE_DCOV, params_bbpw = dict_params_bbpw,
-                            dict_compsep= dict_ells_bbpw,
-                            cros = True, mmt = True, dec = False) # dc0 or dcs, simple
+                            cros = mom, mmt = mom, dec = decs)
--- run_test.py
+++ run_test.py
@@ -4,7 +4,7 @@ first step
 from utils.params import config
 from utils.params import NAME_COMP
 from utils_bbpw.bbpw_script import write_config_yml_script
-from utils_bbpw.params import dict_params_bbpw, dict_ells_bbpw, niterbands
+from utils_bbpw.params import dict_params_bbpw, dict_ells_bbpw #, niterbands
 from dustngwbbp.compute_cl import compute_cl_nobin, compute_cl
 from dustngwbbp.compute_couplingmatrix import compute_couplingmatrix
 from dustngwbbp.compute_cov import compute_cov, get_effective_cov # , get_crazy_cov
@@ -17,21 +17,24 @@ compute_cl_nobin(NAME_COMP)
 compute_cov('d00', w2_mean)
 compute_cov(NAME_COMP, w2_mean)
 
-get_effective_cov()
+get_effective_cov('nobin')
+get_effective_cov('bin')
 
 # get_crazy_cov('offset')
 
 compute_cl(NAME_COMP,'w')
 compute_cl(NAME_COMP,'wt')
 
-for biter in niterbands:
-    dict_ells_bbpw['bands'] = biter
-    for template in ['w', 'wt']:
-        for decs in [True, False]:
-            write_config_yml_script(template, params_bbpw = dict_params_bbpw,
-                                    dict_compsep= dict_ells_bbpw,
-                                    cros = False, mmt = False, dec = decs) # dc0 or dcs, simple
-        if NAME_COMP == 'dcs':
-            write_config_yml_script(template, params_bbpw = dict_params_bbpw,
-                                    dict_compsep= dict_ells_bbpw,
-                                    cros = True, mmt = True, dec = False)
+# for biter in niterbands:
+#     dict_ells_bbpw['bands'] = biter
+dict_ells_bbpw['bands'] = 'all'
+
+for template in ['w', 'wt']:
+    for decs in [True, False]:
+        write_config_yml_script(template, params_bbpw = dict_params_bbpw,
+                                dict_compsep= dict_ells_bbpw,
+                                cros = False, mmt = False, dec = decs) # dc0 or dcs, simple
+    if NAME_COMP == 'dcs':
+        write_config_yml_script(template, params_bbpw = dict_params_bbpw,
+                                dict_compsep= dict_ells_bbpw,
+                                cros = True, mmt = True, dec = False)
