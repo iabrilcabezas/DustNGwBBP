bd4c8b8b646e101ef5f17dce6efb46f0924a3408
Irene Abril Cabezas <79768254+iabrilcabezas@users.noreply.github.com>
2023-04-17 03:37:18 -0700

    also analysis folder

--- bbpower_input/make_directories.sh
+++ bbpower_input/make_directories.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-name_run=230324/
+name_run=230414
 base_folder=/global/cfs/cdirs/act/data/iabril/BBPower/
+base_df=/global/cfs/cdirs/act/data/iabril/DustFilaments/
+sims_name=sims_230306/
 
 cd ${base_folder}
 mkdir ${name_run}
@@ -14,3 +16,7 @@ mkdir results_CL
 mkdir results_pte
 cd results_pte
 mkdir Figures
+
+cd ${base_df}
+cd ${sims_name}
+mkdir analysis_${name_run}
