e4d382c0190ba4d1d48af691655843d78d889f7b
Irene Abril Cabezas <79768254+iabrilcabezas@users.noreply.github.com>
2023-07-21 05:16:52 -0700

    writes BB cell model from best fit

--- pte/generate_cleancellmodel.sh
+++ pte/generate_cleancellmodel.sh
@@ -9,11 +9,11 @@ base_folder=/global/cfs/cdirs/act/data/iabril/BBPower/230414
 rubbish=so_256_w2_p353_30_9_30_soflat_5.0_0.4_10_B_dcs_Cl_w
 
 
-out_base=/global/cfs/cdirs/act/data/iabril/BBPower/230503_pte
+out_base=/global/cfs/cdirs/act/data/iabril/BBPower/230525_sample/sims/ #03_pte
 
 wtypes='w dfwt'
 
-for i in {9..999}
+for i in {10000..29999}
 do
 echo ${i}
 for w_type in $wtypes
@@ -21,7 +21,7 @@ do
 
 outdir=${out_base}/${i}/${w_type}
 
-python -m bbpower BBCompSep --cells_fiducial=${base_folder}/${rubbish}_fid.fits --cells_noise=${base_folder}/${rubbish}_noi.fits --cells_coadded=${base_folder}/${rubbish}_tot.fits  --config=${outdir}/bestfitcell.yml --output_dir=${outdir}
+python -m bbpower BBCompSep --cells_fiducial=${base_folder}/${rubbish}_fid.fits --cells_noise=${base_folder}/${rubbish}_noi.fits --cells_coadded=${base_folder}/${rubbish}_tot.fits --cells_coadded_cov=${base_folder}/${rubbish}_tot.fits --config=${outdir}/bestfitcell.yml --output_dir=${outdir}
 
 done
 done
