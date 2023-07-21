7f1c5ff61fb63fd6e8ae90965e264838676b805d
Irene Abril Cabezas <79768254+iabrilcabezas@users.noreply.github.com>
2023-07-21 05:20:13 -0700

    thought i wanted noise but no

--- run_decorr.py
+++ run_decorr.py
@@ -2,9 +2,10 @@
 prepares decorr data to be analyzed with BBCompSep
 '''
 
-from decorr.add_cov2decorr import add_cov2decorr
+from decorr.add_cov2decorr import add_cov2decorr, add_covandnoise2decorr
 
 TYPE_COV = 'dfwt'
 WEIGHT = 'Cl'
 
 add_cov2decorr(TYPE_COV,WEIGHT)
+#add_covandnoise2decorr()
