311f269ed624b48330646c2856105d01ae3631a2
Irene Abril Cabezas <79768254+iabrilcabezas@users.noreply.github.com>
2023-07-21 04:25:55 -0700

    data with decorrelation

--- decorr/add_cov2decorr.py
+++ decorr/add_cov2decorr.py
@@ -3,11 +3,19 @@ add_cov2decorr
     adds gaussian and non-gaussian cov matrices to data with decorrelation
 '''
 
+import numpy as np
+import copy
 from astropy.io import fits
 import sacc
-from utils.params import PATH_DICT, NAME_RUN, NAME_COMP
-from utils.params import POLARIZATION_cov
+from utils.params import PATH_DICT, NAME_RUN, NAME_COMP, LMIN, DELL
+from utils.params import POLARIZATION_cov, NBANDS, EXPERIMENT
 from utils.sed import get_band_names
+import utils.noise_calc as nc
+from utils.bandpowers import get_ell_arrays
+from dustngwbbp.compute_cl import get_windows, add_tracers, add_powerspectra
+
+LMAX, LARR_ALL, LBANDS, LEFF = get_ell_arrays(LMIN, DELL, NBANDS)
+
 
 band_names = get_band_names()
 nfreqs = len(band_names)
@@ -52,4 +60,70 @@ def add_cov2decorr(type_cov, weight):
     s_fg.save_fits(PATH_DICT['output_path'] + '_'.join([NAME_RUN, weight, 'w']) + \
                     '_tot.fits', overwrite = True)
     s_fng.save_fits(PATH_DICT['output_path'] + '_'.join([NAME_RUN, weight, type_cov]) + \
+                    '_tot.fits', overwrite = True)
+
+def add_covandnoise2decorr():
+    '''
+    reads in cells with decorrelation, and adds covariance and noise! to saccs
+    saves gaussian (w) and non gaussian (type_cov) covariance
+    '''
+
+    # load fiducial power spectrum for NAME_RUN parameters
+    name_sf = PATH_DICT['output_path'] + 'cells_model.fits'
+
+    cell_model = sacc.Sacc.load_fits(name_sf)
+    cell_model_matrix = cell_model.mean.reshape((ncross, NBANDS))
+
+    # cell(nu, nuprime):
+    cell_nunup_ell = np.zeros((nfreqs, nfreqs, NBANDS))
+    for i in range(NBANDS):
+        # https://stackoverflow.com/questions/17527693/transform-the-upper-lower-triangular-part-of-a-symmetric-matrix-2d-array-into
+        X = np.zeros((nfreqs, nfreqs))
+        X[np.triu_indices(X.shape[0], k = 0)] = cell_model_matrix[:,i]
+        X = X + X.T - np.diag(np.diag(X))
+        cell_nunup_ell[:,:, i] = X
+
+    weight = 'Cl'
+    windows = get_windows(weight)
+    fsky = nc.get_fsky()
+
+    if EXPERIMENT == 'so':
+
+        # N_ell
+        sens=2
+        knee=1
+        ylf=1
+        nell=np.zeros([nfreqs,LMAX+1])
+        _,nell[:,2:],_=nc.Simons_Observatory_V3_SA_noise(sens,knee,ylf,fsky,LMAX+1,1, atm_noise = True)
+
+    else:
+        print('only implemented for SO EXPERIMENT')
+        return None
+
+    noise_array=np.sum(nell[:,None,:]*windows[None,:,:],axis=2) # bin
+
+    sn_array = copy.deepcopy(cell_nunup_ell)
+    for ib in range(nfreqs):
+        sn_array[ib,ib,:] += noise_array[ib]
+
+    s_g = add_tracers(LARR_ALL)
+    s_ng = add_tracers(LARR_ALL)
+    s_g = add_powerspectra(s_g, sn_array, LEFF, True, 'Cl')
+    s_ng = add_powerspectra(s_ng, sn_array, LEFF, True, 'Cl')
+
+    # populate with gaussian and NG covariance
+    cov_ng = fits.open(PATH_DICT['input_path'] + NAME_RUN + '_' + \
+                            '_'.join([NAME_COMP,'Cov', 'bin']) + '_dfwt.fits')[0].data
+    cov_ng = cov_ng.reshape([ncross* NBANDS,ncross * NBANDS ])
+    s_ng.add_covariance(cov_ng)
+    # repeat:
+    cov_g = fits.open(PATH_DICT['input_path'] + NAME_RUN + '_' + \
+                            '_'.join([NAME_COMP,'Cov', 'bin']) + '_w.fits')[0].data
+    cov_g = cov_g.reshape([ncross* NBANDS,ncross * NBANDS ])
+    s_g.add_covariance(cov_g)
+
+    # save:
+    s_g.save_fits(PATH_DICT['output_path'] + '_'.join([NAME_RUN, weight, 'w']) + \
+                    '_tot.fits', overwrite = True)
+    s_ng.save_fits(PATH_DICT['output_path'] + '_'.join([NAME_RUN, weight, 'dfwt']) + \
                     '_tot.fits', overwrite = True)
--- decorr/run_decorr_cell.sh
+++ decorr/run_decorr_cell.sh
@@ -1,10 +1,8 @@
 #(BBenv)iabril@nersc:login22 [04:28:57] [/global/cfs/cdirs/act/software/iabril/condaenvs/github_reps/BBPower]
 
-module load python
-conda activate /global/cfs/cdirs/act/software/iabril/condaenvs/BBenv
-cd /global/cfs/cdirs/act/software/iabril/condaenvs/github_reps/BBPower
-
-
+# module load python
+# conda activate /global/cfs/cdirs/act/software/iabril/condaenvs/BBenv
+# cd /global/cfs/cdirs/act/software/iabril/condaenvs/github_reps/BBPower
 
 # ### FIRST PART, GENERATE DATA WITH DECORR
 # # --cells_* can be rubbish
@@ -14,8 +12,8 @@ cd /global/cfs/cdirs/act/software/iabril/condaenvs/github_reps/BBPower
 # rubbish=so_256_w2_p353_30_9_30_soflat_5.0_0.4_10_B_dcs_Cl_w
 
 # config_file=/global/common/software/act/iabril/python/DustNGwBBP/decorr/bbcompsep_decorr.yml
-# outdir=/global/cfs/cdirs/act/data/iabril/BBPower/230503_decorr
-#python -m bbpower BBCompSep --cells_fiducial=${base_folder}/${rubbish}_fid.fits --cells_noise=${base_folder}/${rubbish}_noi.fits --cells_coadded=${base_folder}/${rubbish}_tot.fits  --config=${config_file} --output_dir=${outdir}
+# outdir=/global/cfs/cdirs/act/data/iabril/BBPower/230524_decorr
+# python -m bbpower BBCompSep --cells_fiducial=${base_folder}/${rubbish}_fid.fits --cells_noise=${base_folder}/${rubbish}_noi.fits --cells_coadded=${base_folder}/${rubbish}_tot.fits --cells_coadded_cov=${base_folder}/${rubbish}_tot.fits --config=${config_file} --config_copy=${outdir}/temp/config_copy_generateD.yml --output_dir=${outdir} 
 
 # add covariance to cells:
 #cd /global/common/software/act/iabril/python/DustNGwBBP/
@@ -23,47 +21,27 @@ cd /global/cfs/cdirs/act/software/iabril/condaenvs/github_reps/BBPower
 ## SECOND PART, RUN BBCOMPSEP:
 
 base_folder=/global/cfs/cdirs/act/data/iabril/BBPower/230414
-base_decorr=/global/cfs/cdirs/act/data/iabril/BBPower/230503_decorr
-
-# machine=perl
-# experiment=so
-# nside=256
-# cov_corr=w2
-# lmin=30
-# nbands=9
-# dell=30
-# dellnmt=10
-# pol=B
-# weight=Cl
-
-# mtype=soflat
-# apodeg=5.0
-# # smooth=0.4
-# templ=p353
-# # w_type=dfwt # 00 #w #wt
-# ctype=dcs
-# cross=C
-# moments=0
-# decor=D
-
-# bands=all
-# lminbb=30
-# lmaxbb=300
+base_decorr=/global/cfs/cdirs/act/data/iabril/BBPower/230622_decorr
 
 all=_tot.fits
 noise=_noi.fits 
 fid=_fid.fits
 
-#wtypes='w dfwt' # ' wt '
+# wtypes='w dfwt' # ' wt '
+w_type='dfwt'
 
 name_run=so_256_w2_p353_30_9_30_soflat_5.0_0.4_10_B_dcs_Cl
 config_file=/global/common/software/act/iabril/python/DustNGwBBP/decorr/bbcompsep_decorr.yml
+
 #for w_type in $wtypes
 #do
-w_type='dfwt'
 
-name_cls=${name_run}_${w_type}
+   name_cls=${name_run}_${w_type}
 
-python -m bbpower BBCompSep --cells_coadded=$base_decorr/${name_cls}${all}  --cells_noise=$base_folder/${name_cls}${noise} --cells_fiducial=$base_decorr/cells_model.fits    --config=${config_file}      --output_dir=$base_decorr/chains/${w_type}/    --config_copy=$base_decorr/temp/config_copy_${w_type}.yml
+     if [ ! -d ${base_decorr}/chains/${w_type} ]; then
+             mkdir ${base_decorr}/chains/${w_type}
+     fi
+
+    python -m bbpower BBCompSep --cells_coadded=$base_decorr/${name_cls}${all}  --cells_coadded_cov=$base_decorr/${name_cls}${all}  --config=${config_file}      --output_dir=$base_decorr/chains/${w_type}/    --config_copy=$base_decorr/temp/config_copy_${w_type}.yml --cells_noise=$base_folder/${name_cls}${noise} --cells_fiducial=$base_decorr/cells_model.fits # (no needed for chi2)
 
 #done
