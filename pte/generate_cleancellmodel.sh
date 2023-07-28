39ad4f16df00375c159349a6dc17da2b77e683c9
Irene Abril Cabezas <79768254+iabrilcabezas@users.noreply.github.com>
2023-07-28 06:22:15 -0700

    new run with git pr 21 bbpower

--- pte/generate_cleancellmodel.sh
+++ pte/generate_cleancellmodel.sh
@@ -11,11 +11,11 @@ base_folder=/global/cfs/cdirs/act/data/iabril/BBPower/230414
 rubbish=so_256_w2_p353_30_9_30_soflat_5.0_0.4_10_B_dcs_Cl_w
 
 
-out_base=/global/cfs/cdirs/act/data/iabril/BBPower/230525_sample/sims/ #03_pte
+out_base=/global/cfs/cdirs/act/data/iabril/BBPower/230725/sims/ #03_pte
 
 wtypes='w dfwt'
 
-for i in {10000..29999}
+for i in {50000..54999}
 do
 echo ${i}
 for w_type in $wtypes
@@ -23,7 +23,7 @@ do
 
 outdir=${out_base}/${i}/${w_type}
 
-python -m bbpower BBCompSep --cells_fiducial=${base_folder}/${rubbish}_fid.fits --cells_noise=${base_folder}/${rubbish}_noi.fits --cells_coadded=${base_folder}/${rubbish}_tot.fits --cells_coadded_cov=${base_folder}/${rubbish}_tot.fits --config=${outdir}/bestfitcell.yml --output_dir=${outdir}
+python -m bbpower BBCompSep --cells_fiducial=${base_folder}/${rubbish}_fid.fits --cells_noise=${base_folder}/${rubbish}_noi.fits --cells_coadded=${base_folder}/${rubbish}_tot.fits --config=${outdir}/bestfitcell.yml --output_dir=${outdir}
 
 done
 done
--- pte/run_ml_ptes.sh
+++ pte/run_ml_ptes.sh
@@ -5,7 +5,7 @@
 
 ## --cell-noise and fiducial can be rubbish because i am not using them
 
-base_folder=/global/cfs/cdirs/act/data/iabril/BBPower/230525_sample/sims/
+base_folder=/global/cfs/cdirs/act/data/iabril/BBPower/230725/sims/
 rubbish=so_256_w2_p353_30_9_30_soflat_5.0_0.4_10_B_dcs_Cl_w_fid.fits
 config_file=/global/common/software/act/iabril/python/DustNGwBBP/pte/config_bestfitpte
 name_run=so_256_w2_p353_30_9_30_soflat_5.0_0.4_10_B_dcs_Cl
@@ -13,13 +13,13 @@ bands='LF MF UHF'
 # wtypes='w dfwt'
 w_type=dfwt
 
-for no_sim in {11265..19999}
+for no_sim in {50000..54999}
 do
 
 echo ${no_sim}
 
-python -m bbpower BBCompSep --cells_fiducial=${base_folder}/${rubbish} --cells_noise=${base_folder}/${rubbish} --cells_coadded=${base_folder}/${no_sim}/w/${name_run}_w_tot.fits --cells_coadded_cov=${base_folder}/${no_sim}/w/${name_run}_w_tot.fits --config=${config_file}_all.yml --output_dir=${base_folder}/${no_sim}/w/
-python -m bbpower BBCompSep --cells_fiducial=${base_folder}/${rubbish} --cells_noise=${base_folder}/${rubbish} --cells_coadded=${base_folder}/${no_sim}/${w_type}/${name_run}_${w_type}_tot.fits --cells_coadded_cov=${base_folder}/${no_sim}/${w_type}/${name_run}_${w_type}_tot.fits --config=${config_file}_all.yml --output_dir=${base_folder}/${no_sim}/${w_type}/
+python -m bbpower BBCompSep --cells_fiducial=${base_folder}/${rubbish} --cells_noise=${base_folder}/${rubbish} --cells_coadded=${base_folder}/${no_sim}/w/${name_run}_w_tot.fits --config=${config_file}_all.yml --output_dir=${base_folder}/${no_sim}/w/
+python -m bbpower BBCompSep --cells_fiducial=${base_folder}/${rubbish} --cells_noise=${base_folder}/${rubbish} --cells_coadded=${base_folder}/${no_sim}/${w_type}/${name_run}_${w_type}_tot.fits --config=${config_file}_all.yml --output_dir=${base_folder}/${no_sim}/${w_type}/
 
 # for band in $bands
 # do
